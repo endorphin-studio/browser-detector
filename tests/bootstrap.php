1d5c6c0dfc0f86091e0a4531de53b011fe74bae1
endorphinua <sergey.nekhaenko@gmail.com>
2016-05-25 17:05:36 +0300

    v 2.0
    fix

---
+++ tests/bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Sergey Nehaenko <sergey.nekhaenko@gmail.com>
+ * @license GPL
+ * @copyright Sergey Nehaenko &copy 2016
+ * @version 1.0
+ * @project browser-detector
+ */
+
+$vendor = realpath(__DIR__ . '/../vendor');
+if (file_exists($vendor . '/autoload.php')) {
+    require $vendor . '/autoload.php';
+} else {
+    $vendor = realpath(__DIR__ . '/../../../');
+    if (file_exists($vendor . '/autoload.php')) {
+        require $vendor . '/autoload.php';
+    } else {
+        throw new Exception('Unable to load dependencies');
+    }
+}
+
+?>
