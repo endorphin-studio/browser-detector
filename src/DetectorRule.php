576ff87c2e3be8b7ba9e0da8918e212079936713
endorphinua <sergey.nekhaenko@gmail.com>
2016-06-09 17:42:58 +0300

    3.0.0
    init rules system

---
+++ src/DetectorRule.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Sergey Nehaenko <sergey.nekhaenko@gmail.com>
+ * @license GPL
+ * @copyright Sergey Nehaenko &copy 2016
+ * @version 1.0
+ * @project browser-detector
+ */
+
+namespace EndorphinStudio\Detector;
+
+
+class DetectorRule
+{
+    private $RuleType;
+    private $ObjectProperty;
+    private $ObjectPropertyValue;
+
+    public function __construct(\SimpleXMLElement $xmlData)
+    {
+
+    }
+
+    public static function loadRulesFromFile($path)
+    {
+
+    }
+}
